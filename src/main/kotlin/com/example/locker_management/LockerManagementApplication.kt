f466866e3c6f67b6d951c53252fdc2fa940c037f
Kim Myeong Hwi <103114227+redpanda54@users.noreply.github.com>
2025-03-09 20:20:24 +0900

    Initialization

---
+++ src/main/kotlin/com/example/locker_management/LockerManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.example.locker_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LockerManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<LockerManagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/locker_management/LockerManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.locker_management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LockerManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
